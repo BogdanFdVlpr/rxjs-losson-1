1c1ac33f0411a1f90ca9dc3fe2eaf65de6e35fdc
Bogdan Prokopenko <pb.dvlpr@gmail.com>
2023-03-02 23:30:23 +0200

    fromEvent add stop$ Observable and add in Observe

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import './rxjs-lessons';
+import './canvas';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
